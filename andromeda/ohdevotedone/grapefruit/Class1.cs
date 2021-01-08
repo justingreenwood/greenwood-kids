c88c373af68a122a0f0f2fe155a95ebfcef3977b
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-01-08 11:15:57 -0500

    Coded Crap
    I coded a bit, as did my sister. ( Perry is clearly not only Perry!)

--- andromeda/ohdevotedone/grapefruit/Class1.cs
+++ andromeda/ohdevotedone/grapefruit/Class1.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace grapefruit
-{
-    class Class1
-    { greenwood/perry/pery.foster @gmail.com;
-        grenwood/aurora/11833 corbindrive/
-    }
-}
+﻿ greenwood|perry|11833 cr.drive|fishers|Indiana|46038|perry.foster@gmail.com|?
+grenwood|aurora|11833 cr.drive|fishers|Indiana|46038|aurora.foster @gmail.com|?
+ greenwood|andromeda|11833 cr.drive|fishers|Indiana|46038|andromeda.renee@gmail.com|?
+  greenwood|Kelly|11833 cr.drive|fishers|Indiana|46038|lilchuck@gmail.com|?
+greenwood|Justin|11833 cr.drive|fishers|Indiana|46038|justin.greenwood@gmail.com|?
+  greenwood|spenecer|11833 cr.drive|fishers|Indiana|46038|spencer@gmail.com|?
---
+++ aurora/Anorexic Apple Juice/Aurora's Address Book/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurora_s_Address_Book
+{
+    class Address
+    {
+        public Address()
+        {
+
+        }
+
+        public Address(string line)
+        {
+            var fields = line.Split('|');
+            if (fields.Length == 8)
+            {
+                this.NAME = fields[0];
+                this.Occupation = fields[1];
+                this.Address1 = fields[2];
+                this.CityStateZip = fields[3];
+                this.Phone = fields[6];
+                this.Email = fields[7];
+            }
+            else
+            {
+                throw new Exception("Not enough data!");
+            }
+        }
+}
