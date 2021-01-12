fb1ecef6637d0b381e1257985911021ea6ad9b2f
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-01-12 10:13:13 -0500

    OH MY GOODNESS!
    I did so much, and yet it only says one step. Huh... Maybe it was because it was all in the same basic area? I don't thik it was.... It was mostly likely because I added, and didn't get rid of anything really.....

--- aurora/Anorexic Apple Juice/Aurora's Address Book/Program.cs
+++ aurora/Anorexic Apple Juice/Aurora's Address Book/Program.cs
@@ -11,7 +11,58 @@ namespace Aurora_s_Address_Book
         {
             var addresses = GrabAddresses();
 
-             static List<Address> GrabAddresses()
+            ConsoleKeyInfo KeY;
+            do
+            {
+                Console.Write("Choose [L]ist, a record number or [Q]uit:");
+                KeY = Console.ReadKey();
+                Console.WriteLine();
+                if (KeY.Key == ConsoleKey.L)
+                {
+                    Console.WriteLine("------------------------------------");
+                    for (var i = 0; i < addresses.Count; i++)
+                    {
+                        var address = addresses[i];
+                        Console.WriteLine($"[{i + 1}]");
+                        Console.WriteLine(address);
+                        Console.WriteLine("------------------------------------");
+                    }
+                }
+                else if (int.TryParse(KeY.KeyChar.ToString(), out var TacoBell))
+                {
+                    if (TacoBell > 0 && TacoBell <= addresses.Count)
+                    {
+                        var address = addresses[TacoBell - 1];
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine($"[{TacoBell}]***");
+                        Console.WriteLine(address);
+                        Console.WriteLine("------------------------------------");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"There is no {TacoBell}! What kind of an idiot do you think I am?!");
+                    }
+
+                }
+
+            } while (KeY.Key != ConsoleKey.Q);
+
+
+
+           TakeCrap(addresses);
+        }
+        public static void TakeCrap(List<Address> addresses)
+        {
+            var lines = new List<string>();
+            foreach (var address in addresses)
+            {
+                lines.Add(address.ToFileString());
+            }
+
+            File.WriteAllLines("addresses.txt", lines);
+        }
+
+        static List<Address> GrabAddresses()
             {
                 var linesArray = File.ReadAllLines("MyAddresses.txt");
                 var lines = new List<string>(linesArray);
@@ -25,9 +76,8 @@ namespace Aurora_s_Address_Book
                 return addresses;
             }
 
+            }
 
 
-
-        }
-    }
-}
+         }
+    
