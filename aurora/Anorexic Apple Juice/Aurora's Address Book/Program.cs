8ab7bc1554836776fcea7d40e821685f07285708
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-01-14 10:01:23 -0500

    Loops
     I did loopity loops today. My sibs haven't done their coding yet, but I can't wait to see how much more behind they are than I am.

--- aurora/Anorexic Apple Juice/Aurora's Address Book/Program.cs
+++ aurora/Anorexic Apple Juice/Aurora's Address Book/Program.cs
@@ -40,13 +40,73 @@ namespace Aurora_s_Address_Book
                             Console.WriteLine("------------------------------------");
                             Console.WriteLine($"[{TacoBell}]***");
                             Console.WriteLine(address);
-                            Console.WriteLine("------------------------------------");
-                            Console.WriteLine("What do you want to change?");
-                            var ChangeWhat = Console.ReadLine();
-                            //if (ChangeWhat="A",)
-                            //{
-
-                            //}
+                            ConsoleKeyInfo ChangeWhat;
+                            do
+                            {
+                                Console.WriteLine("------------------------------------");
+                                Console.WriteLine("What do you want to change?");
+                                Console.WriteLine(" [N]ame");
+                                Console.WriteLine(" [A]ddress");
+                                Console.WriteLine(" [O]ccupation");
+                                Console.WriteLine(" [P]hone Number");
+                                Console.WriteLine(" [C]ity State Zip");
+                                Console.WriteLine(" [E]-mail Address");
+                                Console.WriteLine(" [Q]uit");
+                                ChangeWhat = Console.ReadKey();
+                                if (ChangeWhat.Key == ConsoleKey.N)
+                                {
+                                    Console.WriteLine("!");
+                                    Console.WriteLine("Would you mind just typing the name in. Don't forget that it MUST be sorted by last comma first middle.");
+                                    Console.ReadLine();
+                                    Console.WriteLine(" If it is sorted incorrectly, you will pay!");
+                                }
+                                else if (ChangeWhat.Key == ConsoleKey.A)
+                                {
+                                    Console.WriteLine("!");
+                                    Console.WriteLine("What should the address be?");
+                                    Console.ReadLine();
+                                    Console.WriteLine(" Oh, ok!");
+                                    //Tomorrow, I will have to ask my dad to explain more!
+                                }
+                                else if (ChangeWhat.Key == ConsoleKey.Q)
+                                {
+                                    Console.WriteLine("!");
+                                    Console.WriteLine("Quitting in process......");
+                                }
+                                else if (ChangeWhat.Key == ConsoleKey.O) 
+                                {
+                                    Console.WriteLine("!");
+                                    Console.WriteLine("I'm sorry. What is this person's real occupation?");
+                                    Console.ReadLine();
+                                    Console.WriteLine("Alright, I will put that into the system.");
+                                }
+                                else if (ChangeWhat.Key == ConsoleKey.P)
+                                {
+                                    Console.WriteLine("!");
+                                    Console.WriteLine("I apologize for the inconvenience I must have caused for you. What is the correct phone number?");
+                                    Console.ReadLine();
+                                    Console.WriteLine("Right away, sire.");
+                                }
+                                else if (ChangeWhat.Key == ConsoleKey.E)
+                                {
+                                    Console.WriteLine("!");
+                                    Console.WriteLine("I was sure that that was the correct e-mail address... Well, maybe I was wrong. What is the e-mail address?");
+                                    Console.ReadLine();
+                                    Console.WriteLine("Oh, alright.");
+                                }
+                                else if (ChangeWhat.Key == ConsoleKey.C)
+                                {
+                                    Console.WriteLine("!");
+                                    Console.WriteLine("There must be a typo somewhere. Well, don't fret. Just type in the correct city, state, and zip code.");
+                                    Console.ReadLine();
+                                    Console.WriteLine("I thank you for your co-operation during this time. Have a fabulicious day.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("!");
+                                    Console.WriteLine("Either you are testing my patience, or you have the brain of a sun-addled bullfrog during mating season!");
+                                }
+                            } while (ChangeWhat.Key != ConsoleKey.Q);
                         }
                         else
                         {
