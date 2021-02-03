73630a7f71cee4869bf4c8a5583d227b806fa7a5
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-02-03 13:55:21 -0500

    stuffity stuff

---
+++ andromeda/adressbookybook/addressesbookybook/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace addressesbookybook
+{
+    partial class addressstuff
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addressstuff));
+            this.addresses = new System.Windows.Forms.CheckedListBox();
+            this.buttonadd = new System.Windows.Forms.Button();
+            this.buttonremove = new System.Windows.Forms.Button();
+            this.buttonedit = new System.Windows.Forms.Button();
+            this.buttonsave = new System.Windows.Forms.Button();
+            this.buttonquit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addresses
+            // 
+            this.addresses.FormattingEnabled = true;
+            this.addresses.Location = new System.Drawing.Point(12, 12);
+            this.addresses.Name = "addresses";
+            this.addresses.Size = new System.Drawing.Size(401, 184);
+            this.addresses.TabIndex = 0;
+            // 
+            // buttonadd
+            // 
+            this.buttonadd.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonadd.Location = new System.Drawing.Point(12, 212);
+            this.buttonadd.Name = "buttonadd";
+            this.buttonadd.Size = new System.Drawing.Size(75, 25);
+            this.buttonadd.TabIndex = 1;
+            this.buttonadd.Text = "add";
+            this.buttonadd.UseVisualStyleBackColor = true;
+            // 
+            // buttonremove
+            // 
+            this.buttonremove.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonremove.Location = new System.Drawing.Point(93, 212);
+            this.buttonremove.Name = "buttonremove";
+            this.buttonremove.Size = new System.Drawing.Size(75, 25);
+            this.buttonremove.TabIndex = 2;
+            this.buttonremove.Text = "remove";
+            this.buttonremove.UseVisualStyleBackColor = true;
+            // 
+            // buttonedit
+            // 
+            this.buttonedit.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonedit.Location = new System.Drawing.Point(174, 212);
+            this.buttonedit.Name = "buttonedit";
+            this.buttonedit.Size = new System.Drawing.Size(75, 25);
+            this.buttonedit.TabIndex = 3;
+            this.buttonedit.Text = "edit";
+            this.buttonedit.UseVisualStyleBackColor = true;
+            // 
+            // buttonsave
+            // 
+            this.buttonsave.BackColor = System.Drawing.Color.Lime;
+            this.buttonsave.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonsave.Location = new System.Drawing.Point(255, 212);
+            this.buttonsave.Name = "buttonsave";
+            this.buttonsave.Size = new System.Drawing.Size(75, 25);
+            this.buttonsave.TabIndex = 4;
+            this.buttonsave.Text = "save";
+            this.buttonsave.UseVisualStyleBackColor = false;
+            this.buttonsave.Click += new System.EventHandler(this.buttonsave_Click);
+            // 
+            // buttonquit
+            // 
+            this.buttonquit.BackColor = System.Drawing.Color.Red;
+            this.buttonquit.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonquit.Location = new System.Drawing.Point(336, 212);
+            this.buttonquit.Name = "buttonquit";
+            this.buttonquit.Size = new System.Drawing.Size(75, 25);
+            this.buttonquit.TabIndex = 5;
+            this.buttonquit.Text = "quit";
+            this.buttonquit.UseVisualStyleBackColor = false;
+            this.buttonquit.Click += new System.EventHandler(this.buttonquit_Click);
+            // 
+            // addressstuff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 390);
+            this.Controls.Add(this.buttonquit);
+            this.Controls.Add(this.buttonsave);
+            this.Controls.Add(this.buttonedit);
+            this.Controls.Add(this.buttonremove);
+            this.Controls.Add(this.buttonadd);
+            this.Controls.Add(this.addresses);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "addressstuff";
+            this.Text = "Addressbook stuffystuff";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox addresses;
+        private System.Windows.Forms.Button buttonadd;
+        private System.Windows.Forms.Button buttonremove;
+        private System.Windows.Forms.Button buttonedit;
+        private System.Windows.Forms.Button buttonsave;
+        private System.Windows.Forms.Button buttonquit;
+    }
+}
+
---
+++ andromeda/adressbookybook/addressesbookybook/addoredit.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace addressesbookybook
+{
+    partial class addoredit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addoredit));
+            this.labelfirst = new System.Windows.Forms.Label();
+            this.labellast = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelfirst
+            // 
+            this.labelfirst.AutoSize = true;
+            this.labelfirst.Location = new System.Drawing.Point(12, 9);
+            this.labelfirst.Name = "labelfirst";
+            this.labelfirst.Size = new System.Drawing.Size(49, 13);
+            this.labelfirst.TabIndex = 0;
+            this.labelfirst.Text = "firstname";
+            // 
+            // labellast
+            // 
+            this.labellast.AutoSize = true;
+            this.labellast.Location = new System.Drawing.Point(12, 46);
+            this.labellast.Name = "labellast";
+            this.labellast.Size = new System.Drawing.Size(52, 13);
+            this.labellast.TabIndex = 1;
+            this.labellast.Text = "last name";
+            // 
+            // address
+            // 
+            this.address.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.address.FormattingEnabled = true;
+            this.address.Location = new System.Drawing.Point(2, 94);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(330, 134);
+            this.address.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(123, 180);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(42, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(190, 180);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(66, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(15, 180);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(89, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // addoredit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(333, 232);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.labellast);
+            this.Controls.Add(this.labelfirst);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "addoredit";
+            this.Text = "add or edit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelfirst;
+        private System.Windows.Forms.Label labellast;
+        private System.Windows.Forms.ListBox address;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
