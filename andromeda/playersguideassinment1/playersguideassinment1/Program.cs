17817252d5ad246d2839f777b3c36dd9b4367c61
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-03-08 10:20:54 -0500

    Aurora's and Andromeda's changes

---
+++ andromeda/playersguideassinment1/playersguideassinment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playersguideassinment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            Console.ReadKey();
+            Console.WriteLine("I'm sorry, Dave. I'm afraid I can't do that.");
+            
+        }
+    }
+}
---
+++ aurora/Anorexic Apple Juice/EasyAssignment1(Remade)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EasyAssignment1_Remade_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printing Hello World is one of the first steps in our programming adventure.
+            Console.WriteLine("Hello, world!");
+            //Reading the line that they type.
+            Console.ReadLine();
+            //naming an integer
+            int score;
+            //state the score
+            score = 8;
+            //print score
+            Console.WriteLine(score);
+            //naming another int and stating what it equals
+            int PerryCurrentAge = 13;
+            //printing Perry's age
+            Console.WriteLine("Perry is currently "+PerryCurrentAge+" years old.");
+        }
+    }
+}
