72108bfd1e17d74657f67b3382451e0aed0354f8
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-03-09 08:15:25 -0500

    perry

---
+++ perry/perrysbeginningwork/perrysbeginningwork/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perrysbeginningwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, and so is your face.");
+            Console.ReadKey();
+        }
+    }
+}
