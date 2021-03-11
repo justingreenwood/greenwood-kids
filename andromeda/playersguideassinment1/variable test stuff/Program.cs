08f9c30d6fa97b489eaf66d4f1f99de796077c35
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-03-11 13:32:00 -0500

    school work and variable stuff. perry's stuff

---
+++ andromeda/playersguideassinment1/variable test stuff/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable_test_stuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            score = 0;
+            score = 4; 
+            score = 11; 
+            score = -1564;
+            int theMeaningOfLife = 42;
+            int number = 3;
+            Console.WriteLine(number); //console write line is usefull
+            int a = 5;
+            int b = 2;
+            b = a;
+            a = -3;
+            Console.WriteLine(a); 
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
--- perry/perrysbeginningwork/perrysbeginningwork/Program.cs
+++ perry/perrysbeginningwork/perrysbeginningwork/Program.cs
@@ -14,6 +14,30 @@ namespace perrysbeginningwork/*This is the name*/
             /*Hello World is the reason this exists.
              This is a comment.*/
             Console.WriteLine("Hello World, and so is your face.");
+
+            int score;
+            score = 0;
+            score = 4; 
+            score = 11;
+            score = -1564;
+            int theMeaningOfLive = 42;
+
+            int number = 3;
+            Console.WriteLine(number);
+
+            int a = 5;
+            int b = 2;
+            b = a;
+            a = -3;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            //int a, b, c;
+            //a = b = c = 10;
+
+
+
+
             //This is what you press when
             //you are ready to quit.
             Console.ReadKey();
