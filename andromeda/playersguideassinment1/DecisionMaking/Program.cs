c3d3398133de1f734918f3367220753e178c9124
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-03-19 15:49:13 -0400

    Scoolwork

---
+++ andromeda/playersguideassinment1/DecisionMaking/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.WriteLine("Enter your score:");
+            string scoreastext = Console.ReadLine();
+            score = Convert.ToInt32(scoreastext);
+            if (score == 100)
+            {
+                Console.WriteLine("Perfect score! You win!");
+                Console.WriteLine("That's awsome!");
+            }
+            else if (score == 99)
+            {
+                Console.WriteLine("Missed it by THAT much.");
+            }
+            else if (score == 0)
+            {
+                Console.WriteLine("You must have been TRYING to get that score.");
+            }
+            else
+            {
+                Console.WriteLine("Ah, come on! That's just boring. Next time get a more interesting score.");
+            }
+            Console.ReadKey();
+            if (score != 100)
+            {
+                //not used
+            }
+            if (score > 90)
+            {
+                Console.WriteLine("You got an 'A'!");
+            }
+            if (score < 60)
+            {
+                Console.WriteLine("You got an 'F'. Sorry.");
+            }
+            if (score >= 90)
+            {
+                Console.WriteLine("You got an 'A'!");
+            }
+            //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            if (score >= 90)
+            {
+                Console.WriteLine("You got an A!");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("You got an B!");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("You got an C!");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("You got an D!");
+            }
+            else
+            {
+                Console.WriteLine("You got an F!");
+            }
+            Console.ReadKey();
+            //<><><><<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>
+           int Score = 45;
+            int pointsNeededToPass = 100;
+            bool levelComplete = (Score >= pointsNeededToPass);
+            if (levelComplete)
+            {
+                Console.WriteLine("You've beaten the level!");
+            }
+            if (!levelComplete)
+            {
+                Console.WriteLine("You haven't won yet. Better keep trying...");
+            }
+            //@#$#@#$#@#@$#@#@#@#$@#$@#@$#@#$@#@#@$#@#$@#$@$$@#@#@#$@
+            int a;
+            int b;
+            Console.WriteLine("Enter your first number:");
+            string aString = Console.ReadLine();
+            a = Convert.ToInt32(aString);
+            b = 2;
+            int remainder;
+            remainder = a % b;
+            if (remainder == 1)
+            {
+                Console.WriteLine("number is odd");
+            }
+            else
+            {
+                Console.WriteLine("number is even");
+            }
+            int shields = 50;
+            int armor = 20;
+            if (shields <= 0 && armor <= 0)
+                Console.WriteLine("You're dead.");
+            if (shields > 0 || armor > 0)
+                Console.WriteLine("You're still alive! Keep going!");
+            if (shields <= 0)
+            {
+                if (armor <= 0)
+                {
+                    Console.WriteLine("Your shields and armor are both zero! You're dead.");
+                }
+                else
+                {
+                    Console.WriteLine("Sheilds are gone, but armor is keeping you alive!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You still have sheilds left. The world is safe.");
+            }
+            int num1;
+            Console.WriteLine("Enter num:");
+            string text1 = Console.ReadLine();
+            num1 = Convert.ToInt32(text1);
+            int num2;
+            Console.WriteLine("Enter another num:");
+            string text2 = Console.ReadLine();
+            num2 = Convert.ToInt32(text2);
+            if ((num1 <= 0 && num2 >= 0) || (num2 <= 0 && num1 >= 0))
+            {
+                Console.WriteLine("Negative");
+            }
+            else
+            {
+                Console.WriteLine("Positive");
+            }
+            Console.WriteLine((score > 70) ? "You passed!" : "You failed.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ perry/perrysbeginningwork/ifButtsThenPlayerDies/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifButtsThenPlayerDies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your score: ");
+            string scoreText = Console.ReadLine();
+            int score = Convert.ToInt32(scoreText);
+
+            if (score == 100)
+            {
+                Console.WriteLine("You got an A+.");
+            }
+            else if (score != 99)
+            {
+                Console.WriteLine("You did not get 99.");
+            }
+            else if(score >= 90)
+            {
+                Console.WriteLine("You got an A.");
+            }
+            else if(score < 60)
+            {
+                Console.WriteLine("You Failed.");
+            }
+            else if (score == 0)            
+                Console.WriteLine("You purposefully failed.");           
+            else            
+                Console.WriteLine("That is a boring score.");
+
+            int pointsToPass = 93;
+
+
+            bool levelIsComplete;
+            if(!(score < pointsToPass))
+            {
+                levelIsComplete = true;
+            }
+            else
+            {
+                levelIsComplete = false;
+            }
+
+            if(!levelIsComplete)
+            {
+                Console.WriteLine("You have not beaten the level.");
+            }
+            else
+            {
+                Console.WriteLine("You have beaten the level.");
+            }
+
+            Console.WriteLine("Type a number to see if it is odd or even. ");
+            string oddEvenString = Console.ReadLine();
+            int oddEven = Convert.ToInt32(oddEvenString);
+            int answer = oddEven % 2;
+            if(oddEven == 0)
+            {
+                Console.WriteLine(oddEven + " is not either odd or even.");
+            }
+            if (answer == 1)
+            {
+                Console.WriteLine($"{oddEven} is odd.");
+            }
+            else
+            {
+                Console.WriteLine($"{oddEven} is even.");
+            }
+
+            int armor = 50;
+            int shield = 20;
+
+            if (shield <= 0 || armor <= 0)
+            {
+                Console.WriteLine("You are still alive.");
+            }
+            else if (shield <= 0 && armor <= 0)
+            {
+                Console.WriteLine("You lose.");
+            }
+
+            if(shield <= 0)
+            {
+                if(armor <= 0)
+                {
+                    Console.WriteLine("You Lose. :(");
+                }
+                else
+                {
+                    Console.WriteLine("You lost your sheild, but you are still alive.");
+                }
+            }
+
+            Console.WriteLine("Type a number. ");
+            string number1 = Console.ReadLine();
+            int Number1 = Convert.ToInt32(number1);
+
+            Console.WriteLine("Type another number. ");
+            string number2 = Console.ReadLine();
+            int Number2 = Convert.ToInt32(number2);
+
+            if((Number1 < 0 && Number2 < 0) || (Number1 > 0 && Number2 > 0))
+            {
+                Console.WriteLine("The answer is positive.");
+            }
+            else
+            {
+                Console.WriteLine("The answer is negative.");
+            }
+
+            Console.WriteLine((score > 70) ? "You passed." : "You failed.");
+
+            Console.ReadKey();
+        }
+    }
+}
