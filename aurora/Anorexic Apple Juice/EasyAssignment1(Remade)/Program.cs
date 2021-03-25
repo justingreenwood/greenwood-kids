f245a4af8a80dfcda94cb77b123b288e239724d1
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-03-25 09:28:03 -0400

    Aurora
     There was only one try it out.

--- aurora/Anorexic Apple Juice/EasyAssignment1(Remade)/Program.cs
+++ aurora/Anorexic Apple Juice/EasyAssignment1(Remade)/Program.cs
@@ -146,6 +146,8 @@ namespace EasyAssignment1_Remade_
                 //int TotalThingsInForEach = Array.Length();
             }
 
+
+
             string palindrome = "Taco Cat";
             var MeaningofLife = 42;
 
@@ -177,8 +179,8 @@ namespace EasyAssignment1_Remade_
                 this.author = author;
             }
             
-             
-         
+           enum MonthsOfYear { January=1, February=2, March=3, April=4, May=5, June=6, July=7, August=8, September=9, October=10, November=11, December=12};
+            string minimonths = "The month is this month.";
 
         }
     }
