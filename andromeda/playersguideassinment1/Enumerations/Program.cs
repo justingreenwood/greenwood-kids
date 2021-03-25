527fc6fd9d85bef253d98d65c91d0e48ec207dcd
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-03-25 14:33:14 -0400

    stuff

---
+++ andromeda/playersguideassinment1/Enumerations/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    enum monthsInYear { jan = 1, feb = 2, march = 3, april = 4, may = 5, june = 6, july = 7, august = 8, sept = 9, oct = 10, nov = 11, dec = 12 };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayofweek = 3;
+            if (dayofweek == 3)
+                Console.WriteLine("It's Tuesday!");
+            int sunday = 1;
+            int monday = 2;
+            int tuesday = 3;
+            int wednesday = 4;
+            int thursday = 5;
+            int friday = 6;
+            int saturday = 7;
+            int dayOfWeek = tuesday;
+             if (dayOfWeek==tuesday)
+                Console.WriteLine("It's Tuesday!");
+            Console.WriteLine("pick a number between 1 and 12.");
+            string calendar= Console.ReadLine();
+            int date = Convert.ToInt32(calendar);
+            switch (date)
+            {
+                case 1:
+                    Console.WriteLine(monthsInYear.jan);
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine(monthsInYear.feb); 
+                    Console.WriteLine("Febuary");
+                    break;
+                case 3:
+                    Console.WriteLine(monthsInYear.march); 
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine(monthsInYear.april); 
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+                    Console.WriteLine(monthsInYear.may);
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine(monthsInYear.june); 
+                    Console.WriteLine("June");
+                    break;
+                case 7:
+                    Console.WriteLine(monthsInYear.july); 
+                    Console.WriteLine("July");
+                    break;
+                case 8:
+                    Console.WriteLine(monthsInYear.august);
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+                    Console.WriteLine(monthsInYear.sept); 
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+                    Console.WriteLine(monthsInYear.oct);
+                    Console.WriteLine("October");
+                    break;
+                case 11:
+                    Console.WriteLine(monthsInYear.nov); 
+                    Console.WriteLine("November");
+                    break;
+                case 12:
+                    Console.WriteLine(monthsInYear.dec);
+                    Console.WriteLine("December");
+                    break;
+                default:
+                    Console.WriteLine("not option");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ perry/perrysbeginningwork/TheNumberNation/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheNumberNation
+{
+    enum DaysOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+    enum Month { January = 1, February = 2, March = 3, April = 4, May = 5, June = 6, July = 7, August = 8, September = 9, October = 10, November = 11, December = 12};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int dayOfWeek = 3;
+            if (dayOfWeek == 3)
+                Console.WriteLine("It's Tuesday.");
+
+            DaysOfWeek today;
+
+            today = DaysOfWeek.Thursday;
+            int dayasint = (int)DaysOfWeek.Sunday;
+
+            Console.WriteLine("Type number of month it is.");
+            string Answer = Console.ReadLine();
+            int Answers = Convert.ToInt32(Answer);
+            Month answer = (Month)Answers;
+
+            if(answer == Month.January)
+            {
+                Console.WriteLine(Month.January);
+            }
+            else if (answer == Month.February)
+            {
+                Console.WriteLine(Month.February);
+            }
+            else if (answer == Month.March)
+            {
+                Console.WriteLine(Month.March);
+            }
+            else if (answer == Month.April)
+            {
+                Console.WriteLine(Month.April);
+            }
+            else if (answer == Month.May)
+            {
+                Console.WriteLine(Month.May);
+            }
+            else if (answer == Month.June)
+            {
+                Console.WriteLine(Month.June);
+            }
+            else if (answer == Month.July)
+            {
+                Console.WriteLine(Month.July);
+            }
+            else if (answer == Month.August)
+            {
+                Console.WriteLine(Month.August);
+            }
+            else if (answer == Month.September)
+            {
+                Console.WriteLine(Month.September);
+            }
+            else if (answer == Month.October)
+            {
+                Console.WriteLine(Month.October);
+            }
+            else if (answer == Month.November)
+            {
+                Console.WriteLine(Month.November);
+            }
+            else if (answer == Month.December)
+            {
+                Console.WriteLine(Month.December);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
