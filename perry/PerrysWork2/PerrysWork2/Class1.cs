c05c55a32e21e1f7cf70f27f20770456882ebc5f
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-04-05 12:53:34 -0400

    Perry
     Perry did all of his work well. I admire his effort. I cashed this in for him, since he had Algebra.

---
+++ perry/PerrysWork2/PerrysWork2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerrysWork2
+{
+    class Class1
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordcount;
+
+        public Class1(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+        public Class1(string title, string author, int pages, int wordcount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordcount = wordcount;
+        }
+
+
+    }
+}
