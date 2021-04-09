4da1d4d3fd07682c2bb04c66d2f96ff653dd21d5
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-04-09 15:29:02 -0400

    Update Program.cs

--- andromeda/codingassignmentspart2/Tic-Tac-Toe/Program.cs
+++ andromeda/codingassignmentspart2/Tic-Tac-Toe/Program.cs
@@ -10,6 +10,25 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
+            Board board = new Board();
+            WinChecker winChecker = new WinChecker();
+            Renderer renderer = new Renderer();
+            Player player1 = new Player();
+            Player player2 = new Player();
+            while(!winChecker.IsDraw(board)&& winChecker.Check(board)==State.undecided)
+            {
+                renderer.Render(board);
+                Position nextMove;
+                if (board.NextTurn == State.X)
+                    nextMove = player1.GetPosition(board);
+                else
+                    nextMove = player2.GetPosition(board);
+                if (!board.SetState(nextMove, board.NextTurn))
+                    Console.WriteLine("That is not a legal move.");
+            }
+            renderer.Render(board);
+            renderer.RenderResults(winChecker.Check(board));
+            Console.ReadKey();
         }
     }
     public enum State { undecided, X, O, };
@@ -60,8 +79,92 @@ namespace Tic_Tac_Toe
         }
         private bool CheckForWin(Board board,State player)
         {
-            for (int row=0; row<3;row++)
-            
+            for (int row = 0; row < 3; row++)
+                if (AreAll(board, new Position[] { new Position(row, 0), new Position(row, 1), new Position(row, 2) }, player))
+                    return true;
+            for (int column = 0; column < 3; column++)
+                if (AreAll(board, new Position[] { new Position(0, column), new Position(1, column), new Position(2,column) }, player))
+                    return true;
+            if (AreAll(board, new Position[] { new Position(0,0), new Position(1, 1), new Position(2, 2) }, player))
+                return true; 
+            if (AreAll(board, new Position[] { new Position(2, 0), new Position(1, 1), new Position(0, 2) }, player))
+                return true;
+            return false;
+        }
+        private bool AreAll(Board board, Position[] positions, State state)
+        {
+            foreach (Position position in positions)
+                if (board.GetState(position) != state) return false;
+            return true;
+        }
+        public bool IsDraw(Board board)
+        {
+            for (int row = 0; row < 3; row++)
+                for (int column = 0; column < 3; column++)
+                    if (board.GetState(new Position(row, column)) == State.undecided) return false;
+            return true;
+        }
+    }
+    public class Renderer
+    {
+        public void Render(Board board)
+        {
+            char[,] symbols = new char[3, 3];
+            for (int row = 0; row < 3; row++)
+                for (int column = 0; column < 3; column++)
+                    symbols[row, column] = SymbolFor(board.GetState(new Position(row, column)));
+            Console.WriteLine($"{symbols[0, 0]}|{symbols[0, 1]}|{symbols[0, 2]}");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($"{symbols[1, 0]}|{symbols[1, 1]}|{symbols[1, 2]}");
+            Console.WriteLine("---+---+---"); Console.WriteLine($"{symbols[0, 0]}|{symbols[0, 1]}|{symbols[0, 2]}");
+            Console.WriteLine($"{symbols[2, 0]}|{symbols[2, 1]}|{symbols[2, 2]}");
+        }
+        private char SymbolFor(State state)
+        {
+            switch(state)
+            {
+                case State.O: return 'O';
+                case State.X: return 'X';
+                default: return ' ';
+            }
+        }
+        public void RenderResults(State winner)
+        {
+            switch (winner)
+            {
+                case State.O:
+                case State.X:
+                    Console.WriteLine(SymbolFor(winner) + "Wins!");
+                    break;
+                case State.undecided:
+                    Console.WriteLine("Draw!");
+                    break;
+            }
+        }
+    }
+    public class Player
+    {
+       public Position GetPosition(Board board)
+        {
+            int position = Convert.ToInt32(Console.ReadLine());
+            Position desiredCoordinate = PositionForNumber(position);
+            return desiredCoordinate;
+        }
+        private Position PositionForNumber(int position)
+        {
+            switch(position)
+            {
+                case 1: return new Position(2, 0);
+                case 2: return new Position(2, 1);
+                case 3: return new Position(2, 2);
+                case 4: return new Position(1, 0);
+                case 5: return new Position(1, 1);
+                case 6: return new Position(1, 2);
+                case 7: return new Position(0, 0);
+                case 8: return new Position(0, 1);
+                case 9: return new Position(0, 2);
+                default: return null;
+            }
         }
     }
 }
