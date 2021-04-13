a6b650514f787b71255466be5697128f3c329155
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-04-13 10:46:37 -0400

    Schoolwork

---
+++ andromeda/codingassignmentspart2/inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class Polygon
+    {
+        public int NumberOfSide { get; set; }
+        public Polygon()
+        {
+            NumberOfSide = 0;
+        }
+        public Polygon(int numberOfSides)
+        {
+            NumberOfSide = numberOfSides;
+        }
+    }
+    class Square : Polygon
+    {
+        public float Size { get; set; }
+        public Square(float size)
+        {
+            Size = size;
+            NumberOfSide = 4;
+        }
+       // Polygon polygon = new Polygon(3);
+        Square square = new Square(4.5f);
+        Polygon polygon = new Square(4.5f);
+    }
+}
