3bb124ede962d6227a5b07c33d880b1ce557546f
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-04-14 08:22:54 -0400

    stuff

---
+++ andromeda/codingassignmentspart2/polymorphismVMac/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphismVMac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+   class SearchEngine
+   {
+        public virtual string[] Search(string findThis)
+        {
+            return new string[0];
+        }
+        SearchEngine searchEngine1 = new SearchEngine();
+        SearchEngine searchEngine3 = new RBsSearchEngine();
+        string[] defultResults = searchEngine1.Search("hello");
+        string[] rbsResults = searchEngine3.Search("hello");
+    }
+    class RBsSearchEngine: SearchEngine
+    {
+        public override string[] Search (string findThis)
+        {
+            return new string[]
+            {
+                "I found something.",
+                "I found this for you."
+            };
+        }
+    }
+    public abstract class Searchengine
+    {
+        public abstract string[] Search(string findThis);
+    }
+    public new string[] Search(string findThis)
+    {
+        //..
+    }
+}
--- perry/PerrysWork2/structs/Program.cs
+++ perry/PerrysWork2/structs/Program.cs
@@ -11,6 +11,46 @@ namespace structs
         static void Main(string[] args)
         {
 
+
+
+
         }
+
     }
+
+
+
+    class Polygon
+    {
+
+        public int NumberOfSides { get; set; }
+
+        public Polygon()
+        {
+            NumberOfSides = 0;
+        }
+
+        public Polygon(int numberOfSides)
+        {
+            NumberOfSides = numberOfSides;
+        }
+
+
+    }
+
+    class Square : Polygon
+    {
+
+        public float Size { get; set; }
+
+        public Square(float size)
+        {
+            Size = size;
+            NumberOfSides = 4;
+        }
+
+
+    }
+
+
 }
