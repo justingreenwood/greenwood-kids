4af5bb5e15fe486506333ef6cdd17878be6014fc
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-04-15 12:28:46 -0400

    didstuff

---
+++ andromeda/codingassignmentspart2/interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+   public interface IfileWriter
+   {
+       string Extension { get; }
+        void Write(string filename);
+   }
+    public class AnyOldClass: RandomBaseClass, Iinterface1, Iinterface2
+    {
+        //..
+    }
+}
