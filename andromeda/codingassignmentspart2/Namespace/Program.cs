462eb00e532fe3bb3e3ccd9de8490ea699d99d6d
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-04-20 12:08:21 -0400

    Schoolwork

---
+++ andromeda/codingassignmentspart2/Namespace/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+using static System.Console;
+
+namespace Namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            double x = Math.PI;
+            WriteLine(Sin(x));
+            ReadKey();
+           
+        }
+    }
+}
