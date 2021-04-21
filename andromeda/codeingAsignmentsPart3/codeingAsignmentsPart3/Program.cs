cce8ff50d6a8d3f1acfc57cb3dff7038491b2b28
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-04-21 13:29:38 -0400

    school

---
+++ andromeda/codeingAsignmentsPart3/codeingAsignmentsPart3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeingAsignmentsPart3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Add(int a, int b)
+            {
+                return a + b;
+            }
+            Console.WriteLine(Add(1, 1)); 
+            Console.WriteLine(Add(2, 2));
+            Console.WriteLine(Add(3, 7));
+        }
+    }
+}
