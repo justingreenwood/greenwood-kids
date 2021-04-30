5399f2e3bd2269148fb5b4d7d656fa41e55df0f1
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-04-30 12:10:38 -0400

    did some work

---
+++ andromeda/codeingAsignmentsPart3/Overload/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    public class Vector
+    {
+        public double Y { get; set; }
+        public double X { get; set; }
+        public double Z { get; set; }
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+        public static Vector operator *(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+        public static Vector operator /(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+       
+    }
+    class silly
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(1, 5, 6);
+            Vector b = new Vector(0, 7, 9);
+            Vector result = a + b;
+        }
+    }
+}
