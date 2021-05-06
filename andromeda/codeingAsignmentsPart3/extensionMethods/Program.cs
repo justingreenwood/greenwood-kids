d879183f36f0097f6f849e1813f707c6146dc2fa
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-05-06 10:23:54 -0400

    Andromeda and Perry
     Both of these people are mad scientists and geniuses. Unfortunately, they have a less than average sister named Aurora who writes mean things about herself because her younger brother thinks it's soooooooooooooooooooo funny.

---
+++ andromeda/codeingAsignmentsPart3/extensionMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionMethods
+{
+    public static class StringExtensiona
+    {
+        private static Random random = new Random();
+        public static string ToRandomCase(this string text)
+        {
+            string result = "";
+            for(int index = 0; index < text.Length; index++)
+            {
+                if (random.Next(2) == 0)
+                    result += text.Substring(index, 1).ToUpper();
+                else
+                    result += text.Substring(index, 1).ToLower();
+            }
+            return result;
+        }
+    }
+}
---
+++ perry/PerrysWork2/extensionmethods/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionmethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string message = "FArt FAt Hosue";
+            //Console.WriteLine(message.WordCount());
+
+            //string mesg = "I love you. You are poo. You suck. You are fat.";
+            //Console.WriteLine(mesg.SentenceCount());
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class WordCountExtensionString
+    {
+
+        public static int WordCount(this string text)
+        {
+            var house = text.Split(' ');
+            return house.Count();
+        }
+
+        public static int SentenceCount(this string text)
+        {
+            var house = text.Split('.');
+            return house.Count() - 1;
+        }
+        public static int ParagraphCount(this string text)
+        {
+            var house = text.Split('\n');
+            return house.Count() - 1;
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
