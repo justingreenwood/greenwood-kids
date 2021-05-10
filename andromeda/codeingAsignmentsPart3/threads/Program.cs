d1bbfba4ec372b1436db65b12060c45666c8ce82
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-05-10 09:22:03 -0400

    work

---
+++ andromeda/codeingAsignmentsPart3/threads/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Counttonumber);
+            thread1.Start(50); 
+            Thread thread2 = new Thread(Counttonumber);
+            thread2.Start(25);
+            Thread thread3 = new Thread(Counttonumber);
+            thread3.Start(5);
+        }
+        public static void Counttonumber(object input)
+        {
+            for (int index = (int)input; index < 100; index++)
+                Console.WriteLine(index + 1);
+            Console.ReadKey();
+        }
+    }
+}
