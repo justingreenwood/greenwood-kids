4e14766f1fa7a2889054b2cc6ebe879d728a5ed1
Perry Greenwood <76137354+zerrysprinkle@users.noreply.github.com>
2021-05-13 10:05:19 -0400

    AGAIN!
      Perry doesn't log his own work in. Aurora still loves him, though.

---
+++ perry/PerrysWork2/TheDynamics/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace TheDynamics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Something : DynamicObject
+    {
+        private Dictionary<string, string> data;
+
+        public Something(string[]names, string[] age)
+        {
+            data = new Dictionary<string, string>();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                data[names[i]] = age[i];
+            }
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (data.ContainsKey(binder.Name))
+            {
+                result = data[binder.Name];
+                return true;
+            }
+            else
+            {
+                result = null;
+                return false;
+            }
+
+        }
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            if (!data.ContainsKey(binder.Name))
+            {
+                return false;
+            }
+
+            data[binder.Name] = value.ToString();
+            return true;
+        }
+
+    }
+}
